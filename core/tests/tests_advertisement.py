9a0d7b507ae7753754fbb9f041313e522033a7e2
Yghor-Castello <yghorcastello.backend@gmail.com>
2023-09-22 19:38:18 -0300

    atualizacao docstrings

--- core/tests/tests_advertisement.py
+++ core/tests/tests_advertisement.py
@@ -10,8 +10,14 @@ from core.models.models_advertisement import Advertisement
 
 
 class AdvertisementViewTestCase(APITestCase):
+    """
+    Test cases for the Advertisement views.
+    """
 
     def setUp(self):
+        """
+        Set up test data.
+        """
         self.realty = Realty.objects.create(
             guest_limit=4,
             bathroom_count=2,
@@ -19,15 +25,21 @@ class AdvertisementViewTestCase(APITestCase):
             cleaning_fee=150.50,
             activation_date="2023-01-01"
         )
-        self.url_list_create = reverse('realty-list')  
+        self.url_list_create = reverse('realty-list')
         self.url_detail = reverse('realty-detail', args=[self.realty.code_realty])
 
     def test_list_advertisements(self):
-        url = reverse('advertisement-list') 
+        """
+        Test the endpoint for listing advertisements.
+        """
+        url = reverse('advertisement-list')
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_create_advertisement(self):
+        """
+        Test creating a new advertisement.
+        """
         realty = Realty.objects.create(
             code_realty="CODE123",
             guest_limit=5,
@@ -47,12 +59,15 @@ class AdvertisementViewTestCase(APITestCase):
         response = self.client.post(url, data, format='json')
 
     def test_update_advertisement(self):
+        """
+        Test updating an existing advertisement.
+        """
         advertisement = Advertisement.objects.create(
             realty=self.realty,
             platform_name='ExamplePlatform',
             platform_fee=100.00,
         )
-        url = reverse('advertisement-detail', args=[advertisement.pk])  
+        url = reverse('advertisement-detail', args=[advertisement.pk])
         data = {
             'platform_name': 'UpdatedPlatform',
         }
@@ -62,14 +77,17 @@ class AdvertisementViewTestCase(APITestCase):
         self.assertEqual(advertisement.platform_name, 'UpdatedPlatform')
 
     def test_delete_advertisement(self):
+        """
+        Test deleting an advertisement.
+        """
         advertisement = Advertisement.objects.create(
             realty=self.realty,
             platform_name='ExamplePlatform',
             platform_fee=100.00,
         )
-        url = reverse('advertisement-detail', args=[advertisement.pk])  
+        url = reverse('advertisement-detail', args=[advertisement.pk])
         response = self.client.delete(url)
-        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)  
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
         if response.status_code != status.HTTP_405_METHOD_NOT_ALLOWED:
             print(response.data)
--- core/tests/tests_reservation.py
+++ core/tests/tests_reservation.py
@@ -1,34 +1,39 @@
-from decimal import Decimal
 from django.urls import reverse
 from django.test import TestCase
 from rest_framework.test import APIClient
 from rest_framework import status
 
-from core.models import Reservation,  Advertisement, Realty
+from core.models import Reservation, Advertisement, Realty
 
 
 class ReservationViewTestCase(TestCase):
+    """
+    Test cases for the Reservation views.
+    """
 
     def setUp(self):
+        """
+        Set up test data.
+        """
         self.client = APIClient()
 
         self.realty = Realty.objects.create(
-            code_realty="R123456", 
-            guest_limit=4, 
-            bathroom_count=2, 
-            accepts_pets=True, 
+            code_realty="R123456",
+            guest_limit=4,
+            bathroom_count=2,
+            accepts_pets=True,
             cleaning_fee="50.00",
             activation_date="2023-01-01"
         )
-        
+
         self.advertisement = Advertisement.objects.create(
-            realty=self.realty, 
-            platform_name="TestPlatform", 
+            realty=self.realty,
+            platform_name="TestPlatform",
             platform_fee="10.00"
         )
-        
+
         self.reservation_data = {
-            'code_reservation': "ADV0008", 
+            'code_reservation': "ADV0008",
             'advertisement': self.advertisement.id,
             'checkin_date': '2023-10-01',
             'checkout_date': '2023-10-10',
@@ -38,11 +43,17 @@ class ReservationViewTestCase(TestCase):
         }
 
     def test_list_reservations(self):
+        """
+        Test the endpoint for listing reservations.
+        """
         url = reverse('reservation-list')
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_checkin_date_after_checkout_date(self):
+        """
+        Test creating a reservation with a check-in date after the checkout date.
+        """
         self.reservation_data['checkin_date'] = '2023-10-10'
         self.reservation_data['checkout_date'] = '2023-10-01'
         url = reverse('reservation-list')
@@ -52,6 +63,9 @@ class ReservationViewTestCase(TestCase):
         self.assertEqual(response.data['detail'], "Check-in date cannot be after or the same as the check-out date.")
 
     def test_checkin_date_same_as_checkout_date(self):
+        """
+        Test creating a reservation with a check-in date the same as the checkout date.
+        """
         self.reservation_data['checkin_date'] = '2023-10-10'
         self.reservation_data['checkout_date'] = '2023-10-10'
         url = reverse('reservation-list')
@@ -61,6 +75,9 @@ class ReservationViewTestCase(TestCase):
         self.assertEqual(response.data['detail'], "Check-in date cannot be after or the same as the check-out date.")
 
     def test_update_reservation_disabled(self):
+        """
+        Test attempting to update a reservation (PUT method), which should be disabled.
+        """
         self.reservation_data['advertisement'] = self.advertisement
         reservation = Reservation.objects.create(**self.reservation_data)
         url = reverse('reservation-detail', args=[reservation.id])
